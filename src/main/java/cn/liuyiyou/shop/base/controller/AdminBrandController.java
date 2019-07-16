eb5ebc844cb03c47bd1a3eedafec00c3867509a3
liuyiyou <liuyiyou@daojia.com>
2019-07-16 08:19:28 +0800

    品牌管理

--- src/main/java/cn/liuyiyou/shop/base/controller/AdminBrandController.java
+++ src/main/java/cn/liuyiyou/shop/base/controller/AdminBrandController.java
@@ -1,60 +1,45 @@
 package cn.liuyiyou.shop.base.controller;
 
-
 import cn.liuyiyou.shop.base.entity.Brand;
 import cn.liuyiyou.shop.base.service.IBrandService;
 import cn.liuyiyou.shop.common.response.Response;
 import cn.liuyiyou.shop.common.response.Result;
 import cn.liuyiyou.shop.common.vo.PageVo;
-import cn.liuyiyou.shop.common.web.BaseController;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.constraints.NotNull;
-
 
 /***
  *
  * @author: liuyiyou.cn
- * @date: 2019/2/7
+ * @date: 2019/7/12
  * @Copyright 2019 liuyiyou.cn Inc. All rights reserved
  */
-@Controller
-@RequestMapping("/base/brand")
-public class BrandController extends BaseController {
+@RequestMapping("/base/admin/brand")
+@RestController
+public class AdminBrandController {
+
+    private String prefix = "base/brand";
+
 
     @Autowired
     private IBrandService brandService;
 
+    @GetMapping("/list")
+    public ModelAndView list() {
+        return new ModelAndView(prefix + "/brand");
+    }
 
     @PostMapping("/list")
-    @ResponseBody
     public Result<IPage<Brand>> list(@RequestBody @NotNull PageVo pageVo) {
         IPage<Brand> brandByPage = brandService.getBrandByPage(pageVo);
         return Response.success(brandByPage);
     }
-
-
-    @GetMapping("/{brandId}")
-    public Result<Brand> get(@PathVariable("brandId") Long brandId) {
-        Brand brand = brandService.getById(brandId);
-        return Response.success(brand);
-    }
-
-
-    @GetMapping("/getBrandInfos")
-    public Result<Brand> getBrandInfos(@RequestParam("brandId") Integer brandId) {
-        return Response.success(brandService.getById(brandId));
-    }
-
-
 }
-
---
+++ src/main/resources/static/admin/base/brand/brand.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $('body').layout({west__size: 185});
+    queryList();
+});
+
+function queryList() {
+    var columns = [{
+        checkbox: true
+    },
+        {
+            field: 'brandId',
+            title: '品牌ID'
+        },
+        {
+            field: 'brandNameCn',
+            title: '品牌名称'
+        },
+        {
+            field: 'brandIcon',
+            title: '品牌图标'
+        },
+        {
+            field: 'createDate',
+            title: '创建时间'
+        },
+        {
+            field: 'lastUpdate',
+            title: '修改时间'
+        },
+        {
+            field: 'status',
+            title: '状态',
+            align: 'center',
+            formatter: function (value, row, index) {
+                if (value == '0') {
+                    return '<span class="badge badge-primary">正常</span>';
+                } else if (value == '1') {
+                    return '<span class="badge badge-danger">禁用</span>';
+                }
+            }
+        },
+        {
+            title: '操作',
+            align: 'center',
+            formatter: function (value, row, index) {
+                var actions = [];
+                actions.push('<a class="btn btn-success btn-xs ' + editFlag + '" href="#" onclick="edit(\'' + row.userId + '\')"><i class="fa fa-edit"></i>编辑</a> ');
+                actions.push('<a class="btn btn-danger btn-xs ' + removeFlag + '" href="#" onclick="remove(\'' + row.userId + '\')"><i class="fa fa-remove"></i>删除</a> ');
+                return actions.join('');
+            }
+        }];
+    var url = "/base/admin/brand/list";
+    $.initTableJsonParams(columns, url, queryParams);
+}
+
+function queryParams(params) {
+    var param =
+        {
+            pageSize: params.limit,
+            pageNum: params.offset / params.limit + 1,
+            searchValue: params.search,
+            orderByColumn: params.sort,
+            isAsc: params.order,
+        }
+    return param;
+}
